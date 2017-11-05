4a1cd955e34c6521a468b9f062662f6e99680533
Erik Bjercke <erik@novaphon.no>
2017-11-05 15:13:02 +0100

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,26 @@
+
+function pressSpace(target, type) {
+    var press = new KeyboardEvent(type, {"keyCode": 32});
+    if(type == 'keyup'){ target.value += ' ';}
+    target.dispatchEvent(press);    
+}
+
+function type(){
+    var text = document.getElementById('words');
+    var currentword = document.getElementById('currentword');
+    var input = document.getElementById('input');
+
+    if(input.value.indexOf('currentword.innerText') == -1){
+        input.value = currentword.innerText;
+        pressSpace(input, 'keydown');
+        
+        setTimeout( function(){
+            pressSpace(input, 'keyup');
+        }, 200);
+    }
+
+}
+
+
+// document.getElementById('input').addEventListener('keyup', function(evt){ console.log(evt.keyCode); } );
+var handler = setInterval(type, 200);
